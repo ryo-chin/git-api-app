6c7547cb046a6cce72a624f1f5f0db53fb5e68f6
ryo-chin <h031203yama@gmail.com>
2020-04-18 23:29:34 +0900

    feature: primeNgの導入

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
