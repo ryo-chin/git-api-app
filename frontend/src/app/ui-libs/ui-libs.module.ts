c277a85799dc52e45fe38c189ba6af9e4964b9cd
ryo-chin <h031203yama@gmail.com>
2020-05-17 17:25:51 +0900

    feature: buttonを自前のComponentに差し替え

--- frontend/src/app/ui-libs/ui-libs.module.ts
+++ frontend/src/app/ui-libs/ui-libs.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingWaveComponent } from './atoms/loading-wave/loading-wave.component';
+import { ButtonModule } from 'primeng';
+import { ButtonComponent } from './atoms/button/button.component';
 
 
 
 @NgModule({
   declarations: [
-    LoadingWaveComponent
+    LoadingWaveComponent,
+    ButtonComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule
   ],
   exports: [
-    LoadingWaveComponent
+    LoadingWaveComponent,
+    ButtonComponent
   ]
 })
 export class UiLibsModule { }
